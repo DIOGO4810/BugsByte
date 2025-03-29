51442194b57d7d212735725f00cc1c5f33b4e723
2004bdlc <a106809@alunos.uminho.pt>
2025-03-29 04:57:29 +0000

    Api kinda working

--- Pages/HomeScreen.jsx
+++ Pages/HomeScreen.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView, ActivityIndicator } from 'react-native';
+import { createArrayObj, totalget, teste, Api, search_id } from '../API.js';
 
 const HomeScreen = () => {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Usando useEffect para buscar dados assim que o componente for montado
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const result = await Api('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur');
+        setData(result.slice(0, 12));
+        setLoading(false);  // Atualizando o estado de carregamento
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+
+    fetchDataFromAPI();
+  }, []);  // O array vazio significa que isso será executado apenas uma vez
+
+
+
+
+  if (loading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    console.log(error);
+    return (
+      <View style={styles.centered}>
+        <Text>Erro: {error.message}</Text>
+      </View>
+    );
+  }
 
 
 
@@ -26,21 +69,34 @@ const HomeScreen = () => {
             <View style={styles.card}>
               <Text style={styles.cardTitle}>Saldo</Text>
             </View>
-          
-          </View>
 
+          </View>
           <View style={styles.row}>
             <View style={styles.card}>
               <Text style={styles.cardTitle}>Lucro</Text>
             </View>
-           
+
           </View>
+
+          <Text style={styles.title}>Dados Obtidos:</Text>
+          <Text style={styles.cardTitle}>{data[search_id(data, 'ethereum')].current_price}</Text>
+          <Text>{data[0].body}</Text>
+          <Text style={styles.title}>Dados Obtidos:</Text>
+          {data && data.map((item) => (
+            <View key={item.id} style={styles.card}>
+              <Text style={styles.cardTitle}>{item.id}</Text>
+              <Text>{item.price_change_24h.toFixed(2)} %</Text>
+              <Text>{item.current_price.toFixed(3)} </Text>
+            </View>
+          ))}
+
+
 
           <View style={styles.row}>
             <View style={styles.card}>
               <Text style={styles.cardTitle}>Última Compra</Text>
             </View>
-           
+
           </View>
 
         </ScrollView>
@@ -66,7 +122,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#202020',
   },
   footerButton: {
-    width:'45%',
+    width: '45%',
     marginHorizontal: 5,
     backgroundColor: '#202020',
     paddingVertical: 12,
