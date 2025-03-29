41dd678428d3ffe1048f2cf34b225724d041a5c3
DIOGO4810 <ribeiroeribeiro2005@gmail.com>
2025-03-29 00:44:50 +0000

    Inicio da HomePage

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Platform, StatusBar as RNStatusBar } from 'react-native';
+import { NativeRouter, Route, Routes } from 'react-router-native'; // Alterei para NativeRouter e Routes
+import HomeScreen from './Pages/HomeScreen'; 
+import AssetsScreen from './Pages/AssetsScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter> {/* Usando NativeRouter ao invés de Router */}
+      <SafeAreaView style={styles.container}>
+        <Routes>  {/* Usando Routes ao invés de Switch */}
+          <Route path="/" element={<HomeScreen />} />  {/* Usando "element" ao invés de "component" */}
+          <Route path="/assets" element={<AssetsScreen />} /> {/* Defina uma rota para Assets */}
+
+        </Routes>
+      </SafeAreaView>
+    </NativeRouter>
   );
 }
 
@@ -14,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? RNStatusBar.currentHeight : 0,
   },
 });
