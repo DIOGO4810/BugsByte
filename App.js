c22581df15ac0257c94c5bd92d23bef28610b59b
a106909 <a106909@alunos.uminho.pt>
2025-03-29 04:50:46 +0000

    Beggining of Singular Asset page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SingularAsset from './Pages/SingularAsset';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="black" /> {/* Optional: Style the status bar */}
+      <SingularAsset/>
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+    padding: 0,
+    margin: 0,
   },
 });
